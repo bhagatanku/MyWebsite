3b862ef572cc8320e11d877168f61780303db009
Ankit Bhagat <130461518+bhagatanku@users.noreply.github.com>
2024-08-16 09:36:18 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+let movies = [
+    {
+        image: 'https://assets-in.bmscdn.com/promotions/cms/creatives/1717080055549_playcardweb.jpg'
+    },
+    {
+        image: 'https://assets-in.bmscdn.com/promotions/cms/creatives/1722839066478_tlpbms1240x300.jpg'
+    },
+    {
+        image: 'https://assets-in.bmscdn.com/promotions/cms/creatives/1722935655122_monsoonpccweb.jpg'
+    },
+    {
+        image: 'https://assets-in.bmscdn.com/promotions/cms/creatives/1723614074191_freeacceswebweeklystream.jpg'
+    }
+]
